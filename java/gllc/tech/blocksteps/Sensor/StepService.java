f7fe04e7cf5d7af42118b7a846e03abeddb34fa1
sonubee <notifications3972@gmail.com>
2017-07-31 08:56:23 -0700

    Bad Change

--- java/gllc/tech/blocksteps/Sensor/StepService.java
+++ java/gllc/tech/blocksteps/Sensor/StepService.java
@@ -1,17 +1,22 @@
 package gllc.tech.blocksteps.Sensor;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.IBinder;
 import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
-
+import android.os.Process;
 /**
  * Created by bhangoo on 7/30/2017.
  */
@@ -27,6 +32,9 @@ public class StepService extends IntentService implements SensorEventListener, S
     public static int numSteps;
     public static boolean isIntentServiceRunning = false;
 
+    public static final int SCREEN_OFF_RECEIVER_DELAY = 500;
+    private PowerManager.WakeLock mWakeLock = null;
+
     public StepService() {
         super("StepService");
     }
@@ -35,21 +43,91 @@ public class StepService extends IntentService implements SensorEventListener, S
     public void onCreate() {
         super.onCreate(); // if you override onCreate(), make sure to call super().
         // If a Context object is needed, call getApplicationContext() here.
+        Log.i("--All", "FIIIIIIIIIIIIIIIIIINDMEEEE5");
+/*
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
+        PowerManager manager =
+                (PowerManager) getSystemService(Context.POWER_SERVICE);
+        mWakeLock = manager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "--All");
+*/
+        //registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
+    }
 
+    private void registerListener() {
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
 
+        numSteps = 0;
+    }
+
+    private void unregisterListener() {
+        sensorManager.unregisterListener(this);
     }
 
     @Override
+    public void onDestroy() {
+        unregisterReceiver(mReceiver);
+        unregisterListener();
+        mWakeLock.release();
+        stopForeground(true);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i("--All", "FIIIIIIIIIIIIIIIIIINDMEEEE6");
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+        Log.i("--All", "FIIIIIIIIIIIIIIIIIINDMEEEE4");
+        startForeground(Process.myPid(), new Notification());
+        registerListener();
+        mWakeLock.acquire();
+
+        return START_STICKY;
+    }
+
+    public BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i("--All", "onReceive("+intent+")");
+            Log.i("--All", "FIIIIIIIIIIIIIIIIIINDMEEEE2");
+
+            if (!intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                return;
+            }
+
+            Runnable runnable = new Runnable() {
+                public void run() {
+                    Log.i("--All", "FIIIIIIIIIIIIIIIIIINDMEEEE1");
+                    Log.i("--All", "Runnable executing.");
+                    unregisterListener();
+                    registerListener();
+                }
+            };
+
+            new Handler().postDelayed(runnable, SCREEN_OFF_RECEIVER_DELAY);
+        }
+    };
+
+    @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+        Log.i("--All", "FIIIIIIIIIIIIIIIIIINDMEEEE7");
+
         isIntentServiceRunning = true;
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         simpleStepDetector = new StepDetector();
         simpleStepDetector.registerListener(this);
-
         numSteps = 0;
+
         sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+        registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
     }
 
     @Override
