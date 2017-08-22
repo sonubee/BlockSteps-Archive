a47c5a6e1ea9eeefa830aeec41b2a27f9e610061
sonubee <notifications3972@gmail.com>
2017-08-22 08:56:08 -0700

    Revert "Revert This"
    This reverts commit a8b537929ab8d4f5d35cac20eb00dc17ded36c8a.

---
+++ java/gllc/tech/blocksteps/BootBroadcastReceiver.java
@@ -0,0 +1,27 @@
+package gllc.tech.blocksteps;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
+import android.widget.Toast;
+
+import gllc.tech.blocksteps.MyTestService;
+
+import static android.support.v4.content.WakefulBroadcastReceiver.startWakefulService;
+
+/**
+ * Created by bhangoo on 8/11/2017.
+ */
+
+public class BootBroadcastReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Launch the specified service when this message is received
+        Log.i("--All", "Boot Wakeful");
+        Toast.makeText(context, "Boot Wakeful", Toast.LENGTH_LONG).show();
+        Intent startServiceIntent = new Intent(context, MyTestService.class);
+        startWakefulService(context, startServiceIntent);
+
+    }
+}
